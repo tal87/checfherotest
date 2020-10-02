42bafd92ace2f014685af7958e02312f9d760971
Tal Ben Shoam <talbsho@gmail.com>
2020-10-02 12:09:22 -0400

    use padding on the table container

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { getOrdersData } from "../utils/actions";
 import Filter from "./filter";
@@ -7,15 +7,13 @@ import OrdersTable from "./ordersTable";
 import Header from "./header";
 
 const App = () => {
-  // console.log(state);
   const dispatch = useDispatch();
-  // console.log(`orders Length: ${state.orders.orders.length}`);
   useEffect(() => {
     dispatch(getOrdersData());
   }, [dispatch]);
 
   return (
-    <div className="app">
+    <div>
       <Header />
       <Filter />
       <OrdersTable />
