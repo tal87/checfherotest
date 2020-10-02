f7704d03f89a5487f4ec5262cf5e0cad73e5aa77
Tal Ben Shoam <talbsho@gmail.com>
2020-10-02 16:07:02 -0400

    fix header/filters for mobile

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,12 +3,12 @@ import "./style.scss";
 
 const Header = () => {
   return (
-    <>
+    <div className="header-wrapper">
       <div className="top-bar"></div>
       <header>
         <img src="./chefhero_logo_white.png" alt="chefhero logo" />
       </header>
-    </>
+    </div>
   );
 };
 
